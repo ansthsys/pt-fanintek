8879a68fc9978ae82c73ecd2b0071eaf848fde29
ansthsys <azmiythufail91@gmail.com>
2023-06-23 22:36:08 +0700

    feat: create user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::insert([
+            [
+                'nama' => 'Ananda Bayu',
+                'email' => 'bayu@email.com',
+                'npp' => 12345,
+                'npp_supervisor' => '11111',
+                'password' => bcrypt('password')
+            ], [
+                'nama' => 'Supervisor',
+                'email' => 'spv@email.com',
+                'npp' => 11111,
+                'npp_supervisor' => null,
+                'password' => bcrypt('password')
+            ]
+        ]);
     }
 }
