33684739d5d0dea6dbc1a672806ff7baad07212d
ansthsys <azmiythufail91@gmail.com>
2023-06-24 12:27:07 +0700

    feat: create request validation in insert presence

---
+++ app/Http/Requests/InsertPresenceRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class InsertPresenceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => [
+                'required',
+                'string',
+                Rule::in(['IN', 'OUT']),
+            ],
+            'waktu' => 'required|date_format:Y-m-d H:i:s',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'validation errors',
+            'data'      => $validator->errors()
+        ], 400));
+    }
+}
